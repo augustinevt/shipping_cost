377f9f8139afe429d4bc767c13e703b0f591604c
August von Trapp and Brian Grant <brian@>
2016-07-05 10:30:48 -0700

    add Parcel#cost_to_ship and unit tests. All tests passing

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,14 +1,24 @@
 class Parcel
   define_method(:initialize) do |length, width, height, weight, speed|
-    @length = length
-    @width = width
-    @height = height
-    @weight = weight
+    @length = length.to_i
+    @width = width.to_i
+    @height = height.to_i
+    @weight = weight.to_i
     @speed = speed
   end
 
   define_method(:volume) do
     @length * @width * @height
+  end
+
+  define_method(:cost_to_ship) do
+
+    if @speed == "express"
+      ((volume() + @weight) * 0.0654).ceil()
+    else
+      ((volume() + @weight) * 0.0454).ceil()
+    end
+
   end
 
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -10,4 +10,14 @@ describe('Parcel') do
     end
   end
 
+  describe('Parcel#cost_to_ship') do
+    it('should return the cost of shipping based on express formula') do
+      new_parcel = Parcel.new(9, 9, 8, 40, "express")
+      expect(new_parcel.cost_to_ship()).to eq(45)
+    end
+    it('should return the cost of shipping based on reqular formula') do
+      new_parcel = Parcel.new(9, 9, 8, 40, "regular")
+      expect(new_parcel.cost_to_ship()).to eq(32)
+    end
+  end
 end
